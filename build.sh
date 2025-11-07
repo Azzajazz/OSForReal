264ac9ff05cba4b6d29fa61b9e2f1994466472ea
Azzajazz <azzajazz1@gmail.com>
2025-11-07 20:58:53 +1100

    Minimal kernel

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -xe
+
+TOOLCHAIN_PREFIX=./toolchain/gcc-15.2.0-cross/bin
+TARGET=i686-elf
+
+SRC_DIR=./src
+BUILD_DIR=./build
+ISO_DIR=./isodir
+
+CFLAGS="-std=gnu99 -ffreestanding -O2 -Wall -Wextra"
+
+# Build and link the kernel.
+mkdir -p $BUILD_DIR
+$TOOLCHAIN_PREFIX/$TARGET-as $SRC_DIR/boot.s -o $BUILD_DIR/boot.o
+$TOOLCHAIN_PREFIX/$TARGET-gcc -c $SRC_DIR/kernel.c -o $BUILD_DIR/kernel.o $CFLAGS
+$TOOLCHAIN_PREFIX/$TARGET-gcc -T link.ld -o $BUILD_DIR/os-for-real.bin -ffreestanding -O2 -nostdlib $BUILD_DIR/boot.o $BUILD_DIR/kernel.o -lgcc
+
+# Verify multiboot header.
+if grub-file --is-x86-multiboot $BUILD_DIR/os-for-real.bin
+then
+    echo "OK: Multiboot header verification successful"
+else
+    echo "ERROR: Multiboot header verification unsuccessful"
+fi
+
+# Make the iso image
+mkdir -p $ISO_DIR/boot/grub
+cp $BUILD_DIR/os-for-real.bin $ISO_DIR/boot/os-for-real.bin
+cp grub.cfg $ISO_DIR/boot/grub/grub.cfg
+grub-mkrescue -o $BUILD_DIR/os-for-real.iso $ISO_DIR
