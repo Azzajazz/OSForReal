0699c54d48ad7932c4ea9a637273990346de5306
Azzajazz <azzajazz1@gmail.com>
2025-11-08 22:46:59 +1100

    Implement gdt

--- src/kernel.c
+++ src/kernel.c
@@ -6,13 +6,15 @@
 #define UNUSED(x) (void)(x)
 #define PACKED __attribute__((packed))
 
-#include "multiboot.h"
 #include "platform/x86.c"
+#include "boot/boot.c"
 #include "hal/hal.c"
 #include "std/std.c"
 
 void kernel_main(Multiboot_Info *boot_info) 
 {
+    gdt_init();
+    // @Cleanup: Name
 	terminal_initialize();
     serial_init();
 
