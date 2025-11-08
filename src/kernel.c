ac59abb362fba060c794a3d25be8b8ed38f31dfa
Azzajazz <azzajazz1@gmail.com>
2025-11-08 12:01:47 +1100

    Implement terminal scrolling and newlines

--- src/kernel.c
+++ src/kernel.c
@@ -10,6 +10,32 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
 
-	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Line 1\n");
+	terminal_writestring("Line 2\n");
+	terminal_writestring("Line 3\n");
+	terminal_writestring("Line 4\n");
+	terminal_writestring("Line 5\n");
+	terminal_writestring("Line 6\n");
+	terminal_writestring("Line 7\n");
+	terminal_writestring("Line 8\n");
+	terminal_writestring("Line 9\n");
+	terminal_writestring("Line 10\n");
+	terminal_writestring("Line 11\n");
+	terminal_writestring("Line 12\n");
+	terminal_writestring("Line 13\n");
+	terminal_writestring("Line 14\n");
+	terminal_writestring("Line 15\n");
+	terminal_writestring("Line 16\n");
+	terminal_writestring("Line 17\n");
+	terminal_writestring("Line 18\n");
+	terminal_writestring("Line 19\n");
+	terminal_writestring("Line 20\n");
+	terminal_writestring("Line 21\n");
+	terminal_writestring("Line 22\n");
+	terminal_writestring("Line 23\n");
+	terminal_writestring("Line 24\n");
+	terminal_writestring("Line 25\n");
+	terminal_writestring("Line 26\n");
+	terminal_writestring("Line 27\n");
+	terminal_writestring("Line 28\n");
 }
--- src/std/terminal.c
+++ src/std/terminal.c
@@ -22,6 +22,28 @@ void terminal_setcolor(uint8_t color)
 	terminal_color = color;
 }
 
+void _terminal_increment_row(void) {
+    terminal_row += 1;
+    if (terminal_row >= VGA_HEIGHT) {
+        // Scroll the screen up.
+        for (size_t y = 0; y < VGA_HEIGHT - 1; y++) {
+            for (size_t x = 0; x < VGA_WIDTH; x++) {
+                const size_t index = y * VGA_WIDTH + x;
+                terminal_buffer[index] = terminal_buffer[index + VGA_WIDTH];
+            }
+        }
+
+        // Fill the last row with spaces.
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
+
+        // Set the terminal row.
+        terminal_row = VGA_HEIGHT - 1;
+    }
+}
+
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
 	const size_t index = y * VGA_WIDTH + x;
@@ -30,21 +52,29 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c) 
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+    if (c == '\n') {
+        terminal_column = 0;
+        _terminal_increment_row();
+    }
+    else {
+        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+        terminal_column += 1;
+        if (terminal_column >= VGA_WIDTH) {
+            terminal_column = 0;
+            _terminal_increment_row();
+        }
+    }
 }
 
-void terminal_write(const char* data, size_t size) 
+
+void terminal_write(String str) 
 {
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+	for (size_t i = 0; i < str.length; i++)
+		terminal_putchar(str.data[i]);
 }
 
 void terminal_writestring(const char* data) 
 {
-	terminal_write(data, strlen(data));
+    String str = str_literal(data);
+	terminal_write(str);
 }
