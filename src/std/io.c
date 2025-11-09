0d856906d6cba2b1add688a3b4b13d8701c3380b
Azzajazz <azzajazz1@gmail.com>
2025-11-09 19:11:27 +1100

    Support l, h and hh length specifiers on u and x

--- src/std/io.c
+++ src/std/io.c
@@ -15,14 +15,14 @@ void write_string(char *data)
 	write(str);
 }
 
-void put_uint(unsigned int n) {
+void put_uint(uint64_t n) {
     if (n == 0) {
         put_char('0');
         return;
     }
 
-    // unsigned int is at most 2^32 - 1 = 4294967295, which has 10 digits.
-    char digits[10];
+    // unsigned int is at most 2^64 - 1, which has 19 digits
+    char digits[19];
     size_t num_digits = 0;
 
     // Digits will print in reverse order if we print them here.
@@ -39,14 +39,14 @@ void put_uint(unsigned int n) {
     }
 }
 
-void put_hex(unsigned int n) {
+void put_hex(uint64_t n) {
     if (n == 0) {
         put_char('0');
         return;
     }
 
-    // unsigned int is at most 0xffffffff, which has 8 digits.
-    char digits[8];
+    // uint64_t is at most 0xffffffffffffffff, which has 16 digits.
+    char digits[16];
     size_t num_digits = 0;
 
     // Digits will print in reverse order if we print them here.
@@ -88,46 +88,73 @@ void put_int(int n) {
 // --------------------------------------------------
 
 typedef enum {
-    FMT_PRINT_UNKNOWN,
-    FMT_PRINT_CHAR,
-    FMT_PRINT_UINT,
-    FMT_PRINT_INT,
-    FMT_PRINT_HEX,
-    FMT_PRINT_STRING,
+    FMT_PRINT_DT_UNKNOWN,
+    FMT_PRINT_DT_CHAR,
+    FMT_PRINT_DT_UINT,
+    FMT_PRINT_DT_INT,
+    FMT_PRINT_DT_HEX,
+    FMT_PRINT_DT_STRING,
 } Fmt_Print_Data_Type;
+
+typedef enum {
+    FMT_PRINT_LM_NONE,
+    FMT_PRINT_LM_L,
+    FMT_PRINT_LM_H,
+    FMT_PRINT_LM_HH,
+} Fmt_Print_Length_Modifier;
 
 typedef struct {
     Fmt_Print_Data_Type data_type;
+    Fmt_Print_Length_Modifier length_modifier;
 } Fmt_Print_Specifier;
 
 Fmt_Print_Specifier _parse_specifier(char **fmt) {
     Fmt_Print_Specifier specifier = {0};
     for (;;) {
+        // @FIXME: More complete length modifiers.
         switch (**fmt) {
-        // @TODO: Parse the rest of the specifiers.
-        case 'c':
-            specifier.data_type = FMT_PRINT_CHAR;
-            break;
+            case 'h': {
+                *fmt += 1;
+                if (**fmt == 'h') {
+                    specifier.length_modifier = FMT_PRINT_LM_HH;
+                    *fmt += 1;
+                }
+                else {
+                    specifier.length_modifier = FMT_PRINT_LM_H;
+                }
+            } break;
 
-        case 'u':
-            specifier.data_type = FMT_PRINT_UINT;
-            break;
+            case 'l':
+                specifier.length_modifier = FMT_PRINT_LM_L;
+                *fmt += 1;
+                break;
+        }
 
-        case 'd':
-            specifier.data_type = FMT_PRINT_INT;
-            break;
+        switch (**fmt) {
+            // @FIXME: Parse the rest of the specifiers.
+            case 'c':
+                specifier.data_type = FMT_PRINT_DT_CHAR;
+                break;
 
-        case 'x':
-            specifier.data_type = FMT_PRINT_HEX;
-            break;
+            case 'u':
+                specifier.data_type = FMT_PRINT_DT_UINT;
+                break;
 
-        case 's':
-            specifier.data_type = FMT_PRINT_STRING;
-            break;
+            case 'd':
+                specifier.data_type = FMT_PRINT_DT_INT;
+                break;
 
-        default:
-            specifier.data_type = FMT_PRINT_UNKNOWN;
-            break;
+            case 'x':
+                specifier.data_type = FMT_PRINT_DT_HEX;
+                break;
+
+            case 's':
+                specifier.data_type = FMT_PRINT_DT_STRING;
+                break;
+
+            default:
+                specifier.data_type = FMT_PRINT_DT_UNKNOWN;
+                break;
         }
         *fmt += 1;
         break;
@@ -148,34 +175,72 @@ void fmt_print_impl(char *fmt, ...) {
             p_fmt += 1;
             Fmt_Print_Specifier specifier = _parse_specifier(&p_fmt);
             switch (specifier.data_type) {
-            case FMT_PRINT_CHAR: {
-                char next = va_arg(args, int);
-                put_char(next);
-            } break;
+                case FMT_PRINT_DT_CHAR: {
+                    char next = va_arg(args, int);
+                    put_char(next);
+                } break;
 
-            case FMT_PRINT_UINT: {
-                unsigned int next = va_arg(args, unsigned int);
-                put_uint(next);
-            } break;
+                case FMT_PRINT_DT_UINT: {
+                    switch (specifier.length_modifier) {
+                        case FMT_PRINT_LM_NONE: {
+                            uint32_t next = va_arg(args, uint32_t);
+                            put_uint((uint64_t)next);
+                        } break;
 
-            case FMT_PRINT_INT: {
-                int next = va_arg(args, int);
-                put_int(next);
-            } break;
+                        case FMT_PRINT_LM_L: {
+                            uint64_t next = va_arg(args, uint64_t);
+                            put_uint(next);
+                        } break;
 
-            case FMT_PRINT_HEX: {
-                unsigned int next = va_arg(args, unsigned int);
-                put_hex(next);
-            } break;
+                        case FMT_PRINT_LM_H: {
+                            uint16_t next = va_arg(args, uint32_t);
+                            put_uint((uint64_t)next);
+                        } break;
+                        
+                        case FMT_PRINT_LM_HH: {
+                            uint8_t next = va_arg(args, uint32_t);
+                            put_uint((uint64_t)next);
+                        } break;
+                    }
+                } break;
 
-            case FMT_PRINT_STRING: {
-                char *next = va_arg(args, char*);
-                write_string(next);
-            } break;
-            
-            default:
-                write_string("%(UNKNOWN_SPECIFIER)");
-                break;
+                case FMT_PRINT_DT_INT: {
+                    int next = va_arg(args, int);
+                    put_int(next);
+                } break;
+
+                case FMT_PRINT_DT_HEX: {
+                    switch (specifier.length_modifier) {
+                        case FMT_PRINT_LM_NONE: {
+                            uint32_t next = va_arg(args, uint32_t);
+                            put_hex((uint64_t)next);
+                        } break;
+
+                        case FMT_PRINT_LM_L: {
+                            uint64_t next = va_arg(args, uint64_t);
+                            put_hex(next);
+                        } break;
+
+                        case FMT_PRINT_LM_H: {
+                            uint16_t next = va_arg(args, uint32_t);
+                            put_hex((uint64_t)next);
+                        } break;
+                        
+                        case FMT_PRINT_LM_HH: {
+                            uint8_t next = va_arg(args, uint32_t);
+                            put_hex((uint64_t)next);
+                        } break;
+                    }
+                } break;
+
+                case FMT_PRINT_DT_STRING: {
+                    char *next = va_arg(args, char*);
+                    write_string(next);
+                } break;
+                
+                default:
+                    write_string("%(UNKNOWN_SPECIFIER)");
+                    break;
             }
         }
         else {
