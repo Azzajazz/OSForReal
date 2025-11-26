7b62ada69138ca016df97a4b9fefe62568dd125c
Azzajazz <azzajazz1@gmail.com>
2025-11-26 12:29:54 +1100

    Simple test runner

--- src/tests/tests.c
+++ src/tests/tests.c
@@ -1,12 +1,47 @@
 #include "../kernel.c"
 
-void assert(char *file, int line, const char *func, bool condition, char *message);
-#define ASSERT(condition, message) assert(__FILE__, __LINE__, __func__, (condition), (message))
+typedef bool (*Test_Fn)();
+
+typedef struct {
+    char *name;
+    Test_Fn function;
+} Test;
+
+#define TEST(test_name) \
+    (Test){.name = #test_name, .function = test_name}
+
+bool dummy_test() {
+    return true;
+}
+
+Test tests[] = {
+    TEST(dummy_test),
+};
 
 void kernel_test(Multiboot_Info *boot_info) {
     UNUSED(boot_info);
     kernel_init();
 
-    fmt_print("Hello from tests!");
-    for (;;);
+    int passed = 0;
+    int failed = 0;
+
+    for (size_t i = 0; i < ARRAY_LEN(tests); i++) {
+        Test test = tests[i];
+        fmt_print("Running test %s... ", test.name);
+
+        bool result = test.function();
+        if (result) {
+            fmt_print("OK!\n");
+            passed += 1;
+        }
+        else {
+            fmt_print("FAIL!\n");
+            failed += 1;
+        }
+
+        fmt_print("--------------------\n");
+        fmt_print("PASSED: %d, FAILED: %d\n", passed, failed);
+    }
+
+    cpu_halt();
 }
