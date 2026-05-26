e28db88d6efbff990d6d16b4e238c476394760bd
Azzajazz <azzajazz1@gmail.com>
2026-05-26 17:15:27 +1000

    Revert "build: Build a partitioned hard drive."
    This reverts commit 8d6f773a612ddeeefdb384e7a3aafac690ede1f9.

--- custom-tools/copy_drivers/build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-mkdir -p build
-
-gcc src/copy_drivers.c -Wall -Wextra -Werror -std=gnu99 -o build/copy_drivers -g
--- custom-tools/mkpart/src/mkpart.c
+++ custom-tools/mkpart/src/mkpart.c
@@ -1,6 +1,3 @@
-//
-// This is basically a script, so everything here is hard-coded.
-//
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -25,56 +22,27 @@ typedef struct {
 } Partition_Entry;
 
 
-
-#define BOOT_PARTITION_SECTORS 512
-#define HARD_DISK_SECTORS 2048
-#define SECTOR_SIZE 512
-
-uint8_t calculate_checksum(Partition_Header *header, Partition_Entry *boot, Partition_Entry *generic) {
-    uint8_t *header_bytes = (uint8_t *)header;
-    uint8_t *boot_bytes = (uint8_t *)boot;
-    uint8_t *generic_bytes = (uint8_t *)generic;
+uint8_t calculate_checksum(Partition_Header header, Partition_Entry boot, Partition_Entry generic) {
+    uint8_t *header_bytes = (uint8_t *)&header;
+    uint8_t *boot_bytes = (uint8_t *)&boot;
+    uint8_t *generic_bytes = (uint8_t *)&generic;
 
     uint8_t sum = 0;
-    for (size_t i = 0; i < sizeof(*header); i++) {
+    for (size_t i = 0; i < sizeof(header); i++) {
         sum += header_bytes[i];
     }
-    for (size_t i = 0; i < sizeof(*boot); i++) {
+    for (size_t i = 0; i < sizeof(boot); i++) {
         sum += boot_bytes[i];
     }
-    for (size_t i = 0; i < sizeof(*generic); i++) {
+    for (size_t i = 0; i < sizeof(generic); i++) {
         sum += generic_bytes[i];
     }
 
     return -sum;
 }
 
-void write_partition_sector(FILE *f) {
-    Partition_Header header = {
-        .magic = {'P', 'A', 'R', 'T'},
-        .checksum = 0, // To be filled in later.
-        .version = 1,
-        .partition_count = 2,
-    };
-
-    Partition_Entry boot = {
-        .type = PT_BOOT,
-        .sector_start = 1,
-        .sector_count = BOOT_PARTITION_SECTORS,
-    };
-
-    Partition_Entry generic = {
-        .type = PT_GENERIC,
-        .sector_start = boot.sector_start + boot.sector_count,
-        .sector_count = HARD_DISK_SECTORS - BOOT_PARTITION_SECTORS - 1,
-    };
-
-    header.checksum = calculate_checksum(&header, &boot, &generic);
-
-    fwrite(&header, sizeof(header), 1, f);
-    fwrite(&boot, sizeof(boot), 1, f);
-    fwrite(&generic, sizeof(generic), 1, f);
-}
+#define BOOT_PARTITION_SIZE 262144
+#define HARD_DISK_SIZE 1048576
 
 int main(int argc, char **argv) {
     char *program_name = shift_args(&argc, &argv);
@@ -92,7 +60,33 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    write_partition_sector(f);
+    //
+    // This is basically a script, so everything here is hard-coded.
+    //
+    Partition_Header header = {
+        .magic = {'P', 'A', 'R', 'T'},
+        .checksum = 0, // To be filled in later.
+        .version = 1,
+        .partition_count = 2,
+    };
+
+    Partition_Entry boot = {
+        .type = PT_BOOT,
+        .sector_start = 1,
+        .sector_count = BOOT_PARTITION_SIZE / 512,
+    };
+
+    Partition_Entry generic = {
+        .type = PT_GENERIC,
+        .sector_start = boot.sector_start + boot.sector_count,
+        .sector_count = (HARD_DISK_SIZE - BOOT_PARTITION_SIZE - 1) / 512,
+    };
+
+    header.checksum = calculate_checksum(header, boot, generic);
+
+    fwrite(&header, sizeof(header), 1, f);
+    fwrite(&boot, sizeof(boot), 1, f);
+    fwrite(&generic, sizeof(generic), 1, f);
 
     fclose(f);
     return 0;
